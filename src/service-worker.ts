dcf3f7f811a46d2f616d42b2b1299d71c7c5f8a7
kairunhanjun <ayamaja32@gmail.com>
2025-11-07 15:33:48 +0700

    service worker test proc

---
+++ src/service-worker.ts
@@ -0,0 +1,47 @@
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext" />
+/// <reference lib="webworker" />
+/// <reference types="@sveltejs/kit" />
+import { build, files, version } from '$service-worker';
+
+const self = globalThis.self as unknown as ServiceWorkerGlobalScope;
+
+const CACHE_NAME = `cache-${version}`;
+const ASSETS = [...build, ...files];
+
+self.addEventListener('install', (event) => {
+  async function addFilesToCache() {
+		const cache = await caches.open(CACHE_NAME);
+		await cache.addAll(ASSETS);
+	}
+	event.waitUntil(addFilesToCache());
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    (async () => {
+      caches.keys().then(async (keys) => {
+        for (const key of keys) {
+          if (key !== CACHE_NAME) await caches.delete(key);
+        }
+      });
+      if ("navigationPreload" in self.registration) {
+        await self.registration.navigationPreload.enable();
+      }
+    })()
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+   event.respondWith(async function () {
+      var cache = await caches.open('cache');
+      var cachedResponsePromise = await cache.match(event.request);
+      var networkResponsePromise = fetch(event.request);
+      event.waitUntil(async function () {
+         var networkResponse = await networkResponsePromise;
+         await cache.put(event.request, networkResponse.clone());
+      }());
+      return cachedResponsePromise || networkResponsePromise;
+    }());
+});
